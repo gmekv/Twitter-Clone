f300de7249cb2852236f798a8d9d53041712f87b
Giorgi <g.mekvabishvili>
2025-11-04 15:18:02 +0400

    Initial Commit

---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by Giorgi Mekvabishvili on 04.11.25.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
